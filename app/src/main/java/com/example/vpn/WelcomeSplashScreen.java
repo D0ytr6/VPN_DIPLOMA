a10f7777b7e6c997285f1ac2e155a887b6eb5fb6
Dmytro <vasyuk.dima27@gmail.com>
2023-04-30 16:48:38 +0300

    Created frame of ui. Add lottie anim. Working connection. No Stop func.

--- app/src/main/java/com/example/vpn/WelcomeSplashScreen.java
+++ app/src/main/java/com/example/vpn/WelcomeSplashScreen.java
@@ -1,14 +1,29 @@
 package com.example.vpn;
 
+import static com.example.vpn.AppDataParcer.getAppDetails;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeSplashScreen extends AppCompatActivity {
+
+    private String StringGetAppURL = "https://raw.githubusercontent.com/D0ytr6/ToyVPN_Testing/master/appdetails.json";
+    private String StringGetConnectionURL = "https://raw.githubusercontent.com/D0ytr6/ToyVPN_Testing/master/filedetails.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getAppDetails(WelcomeSplashScreen.this, StringGetAppURL, StringGetConnectionURL);
+                finish();
+            }
+        }, 1000);
     }
 }
